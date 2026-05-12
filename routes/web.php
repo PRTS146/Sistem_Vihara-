677613d0f8f45312901e8549b936d2db242611a7
Fadhlan <fadhlanlim@gmail.com>
2026-05-12 21:59:04 +0700

    fix: merapikan posisi file Google Auth dari Kelvin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ViharaController;
+use App\Http\Controllers\GoogleAuthController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -9,6 +10,11 @@ use App\Http\Controllers\ViharaController;
 Route::get('/', [ViharaController::class,'home'])->name("mainpage");
 Route::get('/dashboard', [ViharaController::class,'dashboard'])->name("dashboard");
 Route::get('/abu', [ViharaController::class,'abu'])->name("abu");
-
 // Route::get('/login', [ViharaController::class,'login'])->name("login");
 // Route::get('/register', [ViharaController::class,'register'])->name("register");
+
+// Rute "Login with Google"
+Route::get('auth/google', [GoogleAuthController::class, 'redirectToGoogle'])->name('google.login');
+
+// Rute Callback (Google Cloud Console)
+Route::get('auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
