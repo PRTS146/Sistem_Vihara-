a3646954ef66aa57e8f828f649c7f0f483600615
FlopWopp <stranger441q@gmail.com>
2026-05-21 15:16:01 +0700

    added login with google button on login.auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,6 +43,10 @@
 
 
         </form>
+        <button class="google-btn">
+   <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Logo">
+    <span>Continue with Google</span>
+</button>
       </div>
     </div>
 
