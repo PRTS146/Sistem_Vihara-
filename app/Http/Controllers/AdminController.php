f1a63aac02ce1e24c6557fbe1ea961a32cb15b46
Mr-kelvinLim553 <kelvin.lim.passy@gmail.com>
2026-05-28 14:18:14 +0700

    About AdminController
    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Donation;
+use App\Models\SlotAbu;
 
 class AdminController extends Controller
 {
-    //
+public function index()
+    {
+        // bagian donasi belum di tambahkan
+
+        $totalAcara = Event::count();
+        $totalPeserta = Event::sum('event_counter');
+
+        $slotTersedia = SlotAbu::where('slot_status', 'tersedia')->count();
+        $slotDiambil = SlotAbu::where('slot_status', 'telah diambil')->count();
+        $totalSlot = SlotAbu::count();
+
+        return view('template.monitoring', compact(
+            'totalAcara',
+            'totalPeserta',
+            'slotTersedia',
+            'slotDiambil',
+            'totalSlot'
+            // bagian donasi belum
+        ));
+    }
 }
